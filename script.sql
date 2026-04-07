c7d8430ae4a29dbb10b8229bd4e8b627b1fc83a1
Larissa <Larissagonsantiago@gmail.com>
2026-04-07 10:16:06 -0300

    Implementa Enunciado 3

--- script.sql
+++ script.sql
@@ -28,3 +28,36 @@ BEGIN
 END;
 $$
 
+--Enunciado 3
+DO $$
+DECLARE
+    cur_sobreviventes REFCURSOR;
+    v_total INT;
+
+    v_sexo VARCHAR(10) := 'female';
+    v_nome_tabela VARCHAR(200) := 'titanic_dataset';
+BEGIN
+    OPEN cur_sobreviventes FOR EXECUTE
+    format(
+        '
+        SELECT COUNT(*)
+        FROM %s
+        WHERE survived = 1 AND sex = $1
+        ',
+        v_nome_tabela
+    ) USING v_sexo;
+
+    LOOP
+        FETCH cur_sobreviventes INTO v_total;
+        EXIT WHEN NOT FOUND;
+        IF v_total = 0 THEN
+            RAISE NOTICE '-1';
+        ELSE
+            RAISE NOTICE 'Sobreviventes mulheres: %', v_total;
+        END IF;
+
+    END LOOP;
+
+    CLOSE cur_sobreviventes;
+END;
+$$
